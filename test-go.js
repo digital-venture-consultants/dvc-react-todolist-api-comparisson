b7cdc3a594c774aaafc59cea3745a2d38e84af26
benjaminkiller <benjamin.killer@dvc.ventures>
2024-03-18 12:25:03 +0100

    chore: change list feature component

---
+++ test-go.js
@@ -0,0 +1,32 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+export let options = {
+  stages: [
+    { duration: "30s", target: 500 },
+    { duration: "30s", target: 500 },
+  ],
+};
+
+export default function() {
+  let res = http.post("http://localhost:1337/todo", JSON.stringify({
+    author: "testuser",
+    text: "testpassword",
+  }), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  check(res, {
+    "status is 200": (r) => r.status === 200,
+  });
+
+  res = http.get("http://localhost:3000/todo");
+
+  check(res, {
+    "status is 200": (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
